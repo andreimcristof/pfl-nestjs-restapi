d383f875758745c9a805f22c2d2976bebf7b7b4b
Andrei C <andreimcristof@gmail.com>
2022-10-21 23:16:04 +0200

    docker compose for DB container

---
+++ pfl-nestjs-restapi/src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return 'signed up';
+  }
+
+  signin() {
+    return 'signed in';
+  }
+}
