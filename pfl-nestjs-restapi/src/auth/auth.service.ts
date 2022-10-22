5d9bad2155e1670e0a683b199097e490c1c1965b
Andrei C <andreimcristof@gmail.com>
2022-10-22 11:11:03 +0200

    saving user to DB, validation on insert, error handling

--- pfl-nestjs-restapi/src/auth/auth.service.ts
+++ pfl-nestjs-restapi/src/auth/auth.service.ts
@@ -1,14 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
 @Injectable({})
 export class AuthService {
   /**
    *
    */
   constructor(private readonly prismaService: PrismaService) {}
-  signup() {
-    return 'signed up';
+  async signup(authDto: AuthDto) {
+    // generate password hash
+    const hash = await argon.hash(authDto.password);
+
+    // save user in DB
+    let user;
+    try {
+      user = await this.prismaService.user.create({
+        data: {
+          email: authDto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002')
+          throw new ForbiddenException('Credentials are invalid');
+      }
+      throw error;
+    }
+
+    // return the saved user
+    return user;
   }
 
   signin() {
