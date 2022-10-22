667d208985a6a14b65a0ca33d010ec762c2e2413
Andrei C <andreimcristof@gmail.com>
2022-10-22 09:55:18 +0200

    prisma config and DI

--- pfl-nestjs-restapi/src/auth/auth.service.ts
+++ pfl-nestjs-restapi/src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 @Injectable({})
 export class AuthService {
+  /**
+   *
+   */
+  constructor(private readonly prismaService: PrismaService) {}
   signup() {
     return 'signed up';
   }
